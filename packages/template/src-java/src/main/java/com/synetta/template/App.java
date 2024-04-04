1fc356a0bbaf1d36a3262c810af3be2237e54104
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-04 19:31:44 +0200

    fix(template): let JSX handle the show

--- packages/template/src-java/src/main/java/com/synetta/template/App.java
+++ packages/template/src-java/src/main/java/com/synetta/template/App.java
@@ -1,8 +1,8 @@
 package com.synetta.template;
 
 import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
 import org.graalvm.polyglot.Source;
-import org.graalvm.polyglot.Value;
 import org.graalvm.polyglot.io.IOAccess;
 
 import javafx.application.Application;
@@ -15,15 +15,16 @@ public class App extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    Context context = Context.newBuilder().allowAllAccess(true).allowIO(IOAccess.ALL).build();
+    Context context = Context.newBuilder()
+      .allowAllAccess(true)
+      .allowHostAccess(HostAccess.ALL)
+      .allowIO(IOAccess.ALL)
+      .build();
     context.getBindings("js").putMember("stage", stage);
     
     String bundlePath = getClass().getResource("/index.bundle.mjs").getPath();
     String src = String.format("import start from '%s';start(stage);", bundlePath);
 
     context.eval(Source.newBuilder("js", src, "index.mjs").build());
-
-    stage.setTitle("Hello, JavaFX!");
-    stage.show();
   }
 }
