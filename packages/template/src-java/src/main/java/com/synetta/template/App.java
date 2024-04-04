30ca3769a8d241aef9d7270d8455ab279fa82aa5
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-04 09:52:19 +0200

    feat(template): add main bundle loader

--- packages/template/src-java/src/main/java/com/synetta/template/App.java
+++ packages/template/src-java/src/main/java/com/synetta/template/App.java
@@ -1,5 +1,10 @@
 package com.synetta.template;
 
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+import org.graalvm.polyglot.io.IOAccess;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -10,6 +15,14 @@ public class App extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
+    Context context = Context.newBuilder().allowAllAccess(true).allowIO(IOAccess.ALL).build();
+    context.getBindings("js").putMember("stage", stage);
+    
+    String bundlePath = getClass().getResource("/index.bundle.mjs").getPath();
+    String src = String.format("import start from '%s';start(stage);", bundlePath);
+
+    context.eval(Source.newBuilder("js", src, "index.mjs").build());
+
     stage.setTitle("Hello, JavaFX!");
     stage.show();
   }
