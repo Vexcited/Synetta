82b3a5398f10ce40da5a32dd113eee6c23993b3a
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-13 11:36:40 +0200

    feat: export bridge

--- packages/synetta/src/bridge/index.ts
+++ packages/synetta/src/bridge/index.ts
@@ -1,4 +1,10 @@
-export { default as Scene } from "./scene/Scene.js";
-export { default as Stage } from "./stage/Stage.js";
-export { default as Label } from "./scene/control/Label.js";
-export { default as StackPane } from "./scene/layout/StackPane.js";
+// bridge components
+export { default as Button } from "./components/Button.jsx";
+export { default as Label } from "./components/Label.jsx";
+export { default as PrimaryStage } from "./components/PrimaryStage.jsx";
+export { default as Scene } from "./components/Scene.jsx";
+export { default as StackPane } from "./components/StackPane.jsx";
+export { default as VBox } from "./components/VBox.jsx";
+
+// hooks
+export { useCurrentStage } from "./contexts/CurrentStage.jsx";
