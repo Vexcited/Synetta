a7400b16fad7fac7b6b354166d60e5a3cf3f171a
Mikkel RINGAUD <mikkel@milescode.dev>
2024-04-13 11:35:23 +0200

    fix: setProperty for spreadProps

--- packages/synetta/src/jsx-runtime.ts
+++ packages/synetta/src/jsx-runtime.ts
@@ -1,10 +1,16 @@
 import { createRenderer } from "solid-js/universal";
 import type { Accessor, Component } from "./index.js";
-import type JSBridge from "./bridge/utils/JSBridge.js";
+import JavaObject from "./bindings/java/lang/Object.js";
 
 export declare namespace JSX {
-  type Element = JSBridge;
-  type ArrayElement = Element[];
+  type Element = JavaObject | ArrayElement | (string & {}) | undefined;
+  interface ArrayElement extends Array<Element> {}
+  interface ElementClass {
+    // empty, libs can define requirements downstream
+  }
+  interface ElementAttributesProperty {
+    // empty, libs can define requirements downstream
+  }
   type ElementType = Element | ArrayElement | Component<any> | Accessor<Element>
 
   interface ElementChildrenAttribute {
@@ -25,7 +31,7 @@ export const {
   spread,
   setProp,
   mergeProps
-} = createRenderer<JSBridge>({
+} = createRenderer<JavaObject>({
   createElement(name) {
     throw new TypeError("Couldn't create element for: " + name);
   },
@@ -39,19 +45,18 @@ export const {
   },
 
   setProperty(node, name, value) {
-    throw new TypeError("Couldn't set property: " + name + " to: " + value);
+    if (node instanceof JavaObject) {
+      // @ts-expect-error : we use the power of class' `set` to update the properties.
+      node[name] = value;
+    } else throw new TypeError("(synetta/jsx-runtime): Couldn't 'setProperty' on '" + name + "' to value '" + value + "' : given node is not an instance of JSBridge.");
   },
   
   insertNode(parent, node, anchor) {
-    console.debug('render', parent, node)
-    if(!parent){
-      console.debug('no parent found!', node)
-    }
-    console.debug('inserting node', node);
+    throw new TypeError("(synetta/jsx-runtime): `insertNode` not implemented.");
   },
 
   isTextNode(node) {
-    return false; // Since not implemented.
+    return false;
   },
 
   removeNode(parent, node) {
