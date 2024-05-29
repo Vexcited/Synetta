7031bdcd989c5e8266c13b16a75434dd96343e66
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-29 08:12:48 +0200

    perf(VBox): optimize comparisons on children

--- packages/synetta/src/bridge/components/VBox.tsx
+++ packages/synetta/src/bridge/components/VBox.tsx
@@ -1,64 +1,57 @@
 import VBoxBridge from "../../bindings/javafx/scene/layout/VBox.js";
-import { children, createEffect, createRenderEffect, splitProps, spreadProps, type Component, type JSX } from "../../index.js";
-import type ObservableList from "../../bindings/javafx/collections/ObservableList.js";
-import { Object } from "../../bindings/index.js";
 import getJavaObject from "../../bindings/utils/getJavaObject.js";
+import { Object } from "../../bindings/index.js";
+import { listTryGetChildren } from "../../bindings/utils/list-helpers.js";
 
-const tryToGetChildren = (child: ObservableList<any>, i: number): Object | null => {
-  try {
-    let c = child.get(i);
-    return new Object(c);
-  }
-  catch { return null; }
-}
+import { 
+  children,
+  createEffect,
+  createRenderEffect,
+  splitProps,
+  spreadProps,
+  
+  type Component,
+  type JSX
+} from "../../index.js";
 
-const VBox: Component<{ children?: JSX.Element | JSX.Element[] }> = (props) => {
+const VBox: Component<{ spacing?: number, children?: JSX.Element | JSX.Element[] }> = (props) => {
   const [p, rest] = splitProps(props, ["children"]);
   const childrenMemo = children(() => p.children);
-  const node = new VBoxBridge();
+  
+  const node = VBoxBridge.__new();
+  const nodeChildrenCollection = node.getChildren();
     
   createEffect(() => {
-    const newChildren = childrenMemo.toArray();
-    const oldChildrenLength = node.getChildren().size();
-    
-    const elementsToRemove: any[] = [];
-    for (let i = 0; i < oldChildrenLength; i++) {
-      const oc = tryToGetChildren(node.getChildren(), i);
-      // if there's no original, there's nothing to remove :D
-      if (!oc) continue;
+    const children = (childrenMemo.toArray() as any[]).filter(child => typeof child?._bridged !== "undefined")
+    const childrenHashCodes = new Set(children.map(child => child.hashCode()));
 
-      let keep = false;
+    for (let i = 0; i < children.length; i++) {
+      const nc = getJavaObject(children[i]);
 
-      for (const nc of newChildren) {
-        // should be an instance of bridge, let's roll
-        if (nc instanceof Object) {
-          if (oc.hashCode() === nc.hashCode()) {
-            keep = true;
-            break;
-          }
-        }
-      }
-
-      if (!keep) {
-        elementsToRemove.push(getJavaObject(oc));
+      if (!nodeChildrenCollection.contains(nc).toV8Value()) {
+        nodeChildrenCollection.add(i, nc);
       }
     }
 
-    node.getChildren().removeAll(...elementsToRemove);
+    const bridgesToRemove: Object[] = [];
+    const nodeChildrenCollectionSize = nodeChildrenCollection.size();
 
-    for (let i = 0; i < newChildren.length; i++) {
-      const nc = newChildren[i];
-      const oc = tryToGetChildren(node.getChildren(), i);
+    for (let i = 0; i < nodeChildrenCollectionSize; i++) {
+      const oc = listTryGetChildren(nodeChildrenCollection, i);
+      if (!oc) continue;
 
-      if (nc instanceof Object) {
-        if (oc && oc.hashCode() === nc.hashCode()) continue;
-        node.getChildren().add(i, getJavaObject(nc));
+      if (!childrenHashCodes.has(oc.hashCode())) {
+        bridgesToRemove.push(oc);
       }
+    }
+
+    if (bridgesToRemove.length > 0) {
+      node.getChildren().removeAll(...bridgesToRemove.map(
+        (o) => getJavaObject(o)
+      ));
     }
   });
 
-  console.debug("[VBox]: new VBoxBridge()");
-  
   createRenderEffect(() => spreadProps(node, rest));
   return node;
 };
