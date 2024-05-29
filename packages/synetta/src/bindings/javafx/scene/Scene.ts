87d273eb8d861b05cec10197b6b04473ef75d78d
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-29 08:06:15 +0200

    feat: add getters for Scene bindings

--- packages/synetta/src/bindings/javafx/scene/Scene.ts
+++ packages/synetta/src/bindings/javafx/scene/Scene.ts
@@ -1,23 +1,51 @@
-import JavaObject from "../../java/lang/Object.js";
+import Object from "../../java/lang/Object.js";
+import type JavaValue from "../../utils/JavaValue.js";
 import getJavaObject from "../../utils/getJavaObject.js";
+import Parent from "./Parent.js";
 
-export default class Scene extends JavaObject {
+export default class Scene extends Object {
   // @ts-expect-error : not typed.
   static readonly #Bridge = javafx.scene.Scene;
 
-  /**
-   * Creates a Scene for a specific root Node with a specific size.
-   *
-   * @param root The root node of the scene graph
-   * @param width The width of the scene
-   * @param height The height of the scene
-   */
-  public constructor (root: JavaObject, width: number, height: number) {
-    const bridge = new Scene.#Bridge(getJavaObject(root), width, height);
-    super(bridge);
+  public static __new(root?: Parent, width?: number, height?: number): Scene {
+    const scene = new Scene.#Bridge(getJavaObject(root!), width, height);
+    return new Scene(scene);
   }
 
-  set root (root: JavaObject) {
+  public constructor (_bridged: any) {
+    super(_bridged);
+  }
+
+  /**
+   * NOTE: This gives you directly the bridged instance.
+   * It's not wrapped in a helper class, that's why **it's not recommended to use it directly**.
+   * 
+   * @see https://openjfx.io/javadoc/21/javafx.graphics/javafx/scene/Scene.html#getRoot()
+   */
+  public get root (): any {
+    return getJavaObject(this).getRoot();
+  }
+
+  /**
+   * @see https://openjfx.io/javadoc/21/javafx.graphics/javafx/scene/Scene.html#setRoot(javafx.scene.Parent)
+   */
+  public set root (root: Parent) {
     getJavaObject(this).setRoot(getJavaObject(root));
+  }
+
+  /**
+   * @see https://openjfx.io/javadoc/21/javafx.graphics/javafx/scene/Scene.html#getWidth()
+   */
+  public get width (): number {
+    const width: JavaValue<number> = getJavaObject(this).getWidth();
+    return width.toV8Value();
+  }
+
+  /**
+   * @see https://openjfx.io/javadoc/21/javafx.graphics/javafx/scene/Scene.html#getHeight()
+   */
+  public get height (): number {
+    const height: JavaValue<number> = getJavaObject(this).getHeight();
+    return height.toV8Value();
   }
 }
