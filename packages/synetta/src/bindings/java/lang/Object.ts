cc004735f3b6527cc5c25ad41c264a4a3a8f9f19
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-29 08:08:57 +0200

    feat: add Object instantiation on fallback

--- packages/synetta/src/bindings/java/lang/Object.ts
+++ packages/synetta/src/bindings/java/lang/Object.ts
@@ -14,26 +14,30 @@ import getJavaObject from "../../utils/getJavaObject.js";
  * you should use `getJavaObject(this)`, to make the code more readable.
  */
 export default class Object {
-  public _bridged: any
-  constructor (_bridged: any) {
-    this._bridged = _bridged;
+  // @ts-expect-error : not typed.
+  static readonly #Bridge = java.lang.Object;
+
+  public static __new (): Object {
+    return new Object(new Object.#Bridge());
   }
+
+  constructor (public _bridged: any) {}
 
   /**
    * Returns a hash code value for the object.
    */
   public hashCode (): number {
-    const proxy = getJavaObject(this).hashCode() as JavaValue<number>;
+    const proxy: JavaValue<number> = getJavaObject(this).hashCode();
     return proxy.toV8Value();
   }
 
   public equals (obj: Object): boolean {
-    const proxy = getJavaObject(this).equals(getJavaObject(obj)) as JavaValue<boolean>;
+    const proxy: JavaValue<boolean> = getJavaObject(this).equals(getJavaObject(obj));
     return proxy.toV8Value();
   }
 
   public toString (): string {
-    const proxy = getJavaObject(this).toString() as JavaValue<string>;
+    const proxy: JavaValue<string> = getJavaObject(this).toString();
     return proxy.toV8Value();
   }
 }
