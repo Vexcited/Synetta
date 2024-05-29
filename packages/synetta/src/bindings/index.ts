efb9bb6aeb27a9b8e22928a7858a1a6dc17df70d
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-29 08:07:08 +0200

    feat: export more bindings to default import

--- packages/synetta/src/bindings/index.ts
+++ packages/synetta/src/bindings/index.ts
@@ -1,1 +1,6 @@
 export { default as Object } from "./java/lang/Object.js";
+export { default as Scene } from "./javafx/scene/Scene.js";
+export { default as Parent } from "./javafx/scene/Parent.js";
+export { default as Node } from "./javafx/scene/Node.js";
+export { default as Stage } from "./javafx/stage/Stage.js";
+export { default as Window } from "./javafx/stage/Window.js";
